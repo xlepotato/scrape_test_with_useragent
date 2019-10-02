4e3ad2a0f6a070aa265dc562cb9aaeaf16e7e32a
xlepotato <yucia181@gmail.com>
2019-10-02 12:19:36 +0800

    general web scrape web service

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 import flask
 from flask import request, jsonify
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
 
 # Create some test data for our catalog in the form of a list of dictionaries.
 books = [
@@ -23,6 +30,9 @@ books = [
      'published': '1975'}
 ]
 
+url = "https://en.wikipedia.org/wiki/Artificial_intelligence"
+
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -34,6 +44,30 @@ def home():
 def api_all():
     return jsonify(books)
 
+@app.route('/api/v1/resources/currency/all', methods=['GET'])
+def api_currencies():
+    try:
+        page = urllib.request.urlopen(url) # conntect to website
+    except:
+        print("An error occured.")
+    soup = BeautifulSoup(page, 'html.parser')
+    print(soup.prettify) # gives the visual representation of the parse tree created from the raw HTML content.
+    regex = re.compile('^tocsection-')
+    content_lis = soup.find_all('li', attrs={'class': regex})
+    print(content_lis)
+    content = []
+    for li in content_lis:
+        content.append(li.getText().split('\n')[0])
+    print(content)
+    return jsonify(content)
+
+    # # Getting the keywords section
+    # keyword_section = soup.find(class_="keywords-section")
+    # # Same as: soup.select("div.article-wrapper grid row div.keywords-section")
+    #
+    # # Getting a list of all keywords which are inserted into a keywords list in line 7.
+    # keywords_raw = keyword_section.find_all(class_="keyword")
+    # keyword_list = [word.get_text() for word in keywords_raw]
 
 @app.route('/api/v1/resources/books', methods=['GET'])
 def api_id():
